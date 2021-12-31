71f59f3098526408f3cfc861e98c7bd8a97d62b6
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-31 16:41:31 +0000

    Fix #12 added basic tests for all routes (http gets)
    TODO add pytest fixure for logged in tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,6 +9,81 @@ def client():
         yield client
 
 
-def test_homepage_200_ok(client):
+def test_get_homepage_200_ok(client):
     req = client.get("/")
     assert req.status_code == 200
+
+
+def test_get_login_200_ok(client):
+    req = client.get("/login")
+    assert req.status_code == 200
+
+
+def test_get_reqest_date_time_200_ok(client):
+    req = client.get("/request-date-time")
+    assert req.status_code == 200
+
+
+def test_get_deposit_200_ok(client):
+    req = client.get("/deposit")
+    assert req.status_code == 200
+
+
+def test_get_create_checkout_session_302_redirect(client):
+    req = client.get("/create-checkout-session")
+    assert req.status_code == 302
+
+
+def test_get_cancel_302_redirect(client):
+    req = client.get("/cancel")
+    assert req.status_code == 302
+
+
+def test_get_admin_deposits_302_redirect(client):
+    req = client.get("/admin/deposits")
+    assert req.status_code == 302
+
+
+def test_get_admin_collected_deposits_302_redirect(client):
+    req = client.get("/admin/collected-deposits")
+    assert req.status_code == 302
+
+
+def test_get_admin_cancelled_bookings_302_redirect(client):
+    req = client.get("/admin/cancelled-bookings")
+    assert req.status_code == 302
+
+
+def test_get_admin_reschedule_302_redirect(client):
+    req = client.get("/admin/reschedule")
+    assert req.status_code == 302
+
+
+def test_get_admin_save_rescheduled_deposit_302_redirect(client):
+    req = client.get("/admin/save-rescheduled-deposit")
+    assert req.status_code == 302
+
+
+def test_get_admin_cancel_booking_302_redirect(client):
+    req = client.get("/admin/cancel-booking")
+    assert req.status_code == 302
+
+
+def test_get_admin_refund_deposit_302_redirect(client):
+    req = client.get("/admin/refund-deposit")
+    assert req.status_code == 302
+
+
+def test_get_admin_charge_deposit_302_redirect(client):
+    req = client.get("/admin/charge-deposit")
+    assert req.status_code == 302
+
+
+def test_get_admin_refunded_deposits_302_redirect(client):
+    req = client.get("/admin/refunded-deposits")
+    assert req.status_code == 302
+
+
+def test_get_admin_302_redirect(client):
+    req = client.get("/admin")
+    assert req.status_code == 302
