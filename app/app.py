aed8a06de4300cd93eca3ccacf3d8dfc14789c74
elliottmotson <elliottmotson@gmail.com>
2022-01-24 13:58:27 +0000

    Broken comment hotfix, initial filter implementation

--- app/app.py
+++ app/app.py
@@ -70,8 +70,8 @@ def set_date_time():
 
 @app.route("/deposit", methods=["GET", "POST"])
 def deposit():
-    return render_template("deposit.html")                                      """TODO - Will need to pass chosen product metadata to this page, to display price dynamically.
-                                                                                   Currently listed as '&pound;15.' - deposit.html - ln 06"""
+    deposit = session["deposit"]
+    return render_template("deposit.html", )                                      #"""TODO - Will need to pass chosen product metadata to this page, to display price dynamically.
 
 
 @app.route("/create-checkout-session", methods=["GET", "POST"])
